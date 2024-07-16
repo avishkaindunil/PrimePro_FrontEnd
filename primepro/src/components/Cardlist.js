f9b720cce86af3a5d065be56ca97740832e71404
IsinduTharudinitha <tharudinitha@gmail.com>
2024-07-16 00:00:53 -0700

    changed the images of the news page

--- primepro/src/components/Cardlist.js
+++ primepro/src/components/Cardlist.js
@@ -3,19 +3,19 @@ import Card from './Card';
 
 const cardData = [
   {
-    image: "https://images.pexels.com/photos/14231678/pexels-photo-14231678.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    image: "https://images.pexels.com/photos/6872580/pexels-photo-6872580.jpeg?auto=compress&cs=tinysrgb&w=600",
     date: "March 28, 2024",
     title: "Fintrex Finance Partners with PrimePro for corporate travel solutions",
     description: "PrimePro, Sri Lanka's leading technology driven mobility solutions provider announced its most recent partnership with Fintrex Finance Limited."
   },
   {
-    image: "https://images.pexels.com/photos/14231678/pexels-photo-14231678.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    image: "https://images.pexels.com/photos/3354648/pexels-photo-3354648.jpeg?auto=compress&cs=tinysrgb&w=600",
     date: "March 28, 2024",
     title: "Fintrex Finance Partners with PrimePro for corporate travel solutions",
     description: "PrimePro, Sri Lanka's leading technology driven mobility solutions provider announced its most recent partnership with Fintrex Finance Limited."
   },
   {
-    image: "https://images.pexels.com/photos/14231678/pexels-photo-14231678.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    image:"https://images.pexels.com/photos/6873179/pexels-photo-6873179.jpeg?auto=compress&cs=tinysrgb&w=600",
     date: "March 28, 2024",
     title: "Fintrex Finance Partners with PrimePro for corporate travel solutions",
     description: "PrimePro, Sri Lanka's leading technology driven mobility solutions provider announced its most recent partnership with Fintrex Finance Limited."
