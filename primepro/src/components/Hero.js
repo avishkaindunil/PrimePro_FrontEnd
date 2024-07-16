b748e50f547fdd428c5f11bbecb1a7e7b609b1d2
binalitanya <binaliukwatte@gmail.com>
2024-07-16 23:38:46 +0530

    task/add comon Hero section

--- primepro/src/components/Hero.js
+++ primepro/src/components/Hero.js
@@ -1,18 +1,18 @@
 
 import React from "react";
 
-function Hero() {
+function Hero({content}) {
   return (
     <div className="w-full mt-10 h-[700px] ">
       <div className="relative h-[700px]">
         <div className="absolute top-0 left-0 w-full h-full bg-black/40 flex flex-col justify-center items-start text-white px-10">
-          <h1 className="text-7xl font-bold mb-4 ml-60">News</h1>
+          <h1 className="text-7xl font-bold mb-4 ml-60">{content.title}</h1>
           <p className="text-xl ml-60">
-            Stay updated with PrimePro's latest services, features, and offers.
+            {content.subTitle}
           </p>
         </div>
         <img
-          src="https://images.pexels.com/photos/14231678/pexels-photo-14231678.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          src={content.image}
           alt="Car washing"
           className="w-full h-full object-cover"
         />
--- primepro/src/pages/root/Home.js
+++ primepro/src/pages/root/Home.js
@@ -4,12 +4,18 @@ import Hero from "../../components/Hero";
 import CardsList from "../../components/Cardlist";
 import Footer from "../../components/Footer";
 
+const heroContent = {
+    title: "NEWS",
+    subTitle: "Stay updated with PrimePro's latest services, features, and offers.",
+    image: "https://images.pexels.com/photos/14231678/pexels-photo-14231678.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+};
+
 const Home = () => {
   return (
     <>
       {/* <Navbar /> */}
       <div className="relative z-[-1]">
-        <Hero />
+        <Hero content={heroContent} />
       </div>
       <div className="pt-10">
         <CardsList />
